db4e79b9a3248f4d52ef93ee54d493f7e5ee2cc3
oct-sky-out <kms3335k@naver.com>
2021-12-19 03:28:15 +0900

    Updates

---
+++ precache-manifest.af3ee78e1f76046901699c9c94e02027.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85af6093495e6a24f3169cdf92b13a4a",
+    "url": "/Weather-project/index.html"
+  },
+  {
+    "revision": "3d94a9f59d1a539b9de6",
+    "url": "/Weather-project/static/css/main.f27317f0.chunk.css"
+  },
+  {
+    "revision": "b6593a4987067d3c366f",
+    "url": "/Weather-project/static/js/2.b49f9c4e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Weather-project/static/js/2.b49f9c4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d94a9f59d1a539b9de6",
+    "url": "/Weather-project/static/js/main.ad9746ae.chunk.js"
+  },
+  {
+    "revision": "6b787e001aad81f45a92",
+    "url": "/Weather-project/static/js/runtime-main.e8258ce8.js"
+  }
+]);
